37e0afafbcf691b136c14358d2726e9a6bb64119
ibai <ibailarra9@gmail.com>
2023-02-27 09:33:04 +0000

    Cambios en stingCalculator

--- src/stringCalculator/src/StringCalculator.php
+++ src/stringCalculator/src/StringCalculator.php
@@ -6,6 +6,13 @@ class StringCalculator
 {
     public function add(String $numbers): int
     {
+        if(strlen($numbers )== 1){
+            return int($numbers);
+        }
+        elseif (strlen($numbers)==2){
+            $splitedNumbers = explode(",",$numbers);
+            return (int($splitedNumbers[0]) + int($splitedNumbers[1]));
+        }
         return 0;
     }
 }
